a2c94dd009019376f449db80b6508e1a6168048f
KwonMinee <bmuscotty214@gmauil.com>
2026-07-03 11:28:20 +0900

    전체 포스트 가져오기 API완료

--- router/posts.mjs
+++ router/posts.mjs
@@ -1,15 +1,20 @@
 import express from "express"
+import { isAuth } from "../middleware/auth.mjs"
+import * as postController from "../controller/posts.mjs"
 
 const router = express.Router()
 
 //전체 포스트 가져오기
+//http://127.0.0.1:8080/post (GET)
 
+router.get("/", isAuth,postController.getPosts)
 
 //글 번호에 대한 포스트만 가져오기
 
 
 //포스트 쓰기
-
+//http://127.0.01:8080/post
+router.post("/",isAuth,postController.createPost)
 
 //포스트 수정하기
 
